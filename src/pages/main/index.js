b58f0602e827a0d16b6b21b96dbd968ab8233804
Isaac Alencar <alencarisaac3@gmal.com>
2020-03-28 12:31:10 -0300

    adiciona paginação da aplicação

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -7,17 +7,42 @@ import './styles.css';
 export default class Main extends Component {
 
     state = {
-        products: []
+        products: [],
+        productInfo: {}, 
+        page: 1
     };
 
     componentDidMount() {//Método executado assim que o comp. é exibido em tela!
         this.loadProducts(); //método próprio que será implementado apra exibir prod.
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
 
-        this.setState({ products: response.data.docs });
+        const { docs, ...productInfo } = response.data
+
+        this.setState({ products: docs, productInfo, page });
+    };
+
+    prevPage = () => {
+        const { page, productInfo } = this.state
+
+        if(page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+
+    };
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
     };
 
     render() {
@@ -36,6 +61,10 @@ export default class Main extends Component {
                         </article>
                     )
                 })}
+                <div className="actions">
+                    <button onClick={this.prevPage}>Anterior</button>
+                    <button onClick={this.nextPage}>Próximo</button>
+                </div>
             </div>
         )
     }
